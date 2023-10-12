b80eb0692ee84d5929cc6b3f73854d2f7951e77d
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2023-10-12 10:43:19 +0700

    Update install_debian10.sh

--- install_debian10.sh
+++ install_debian10.sh
@@ -2,9 +2,7 @@ sudo apt-get update
 wget -O- https://repo.3cx.com/key.pub | sudo apt-key add
 echo "deb http://repo.3cx.com/3cx buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt-get update
-apt install gnupg
-apt update && apt install net-tools dphys-swapfile htop mtr screen curl dnsutils -y
-apt-cache policy 3cxpbx | grep -o '18.*' | grep -o '^\S*'
-echo "Select the Version to intall [PRESS ENTER]: " 
-read version
-sudo apt-get install 3cxpbx=$version
+sudo apt install gnupg
+sudo apt-get update 
+sudo apt install net-tools dphys-swapfile htop mtr screen curl dnsutils -y
+sudo apt list policy 3cxpbx -a
